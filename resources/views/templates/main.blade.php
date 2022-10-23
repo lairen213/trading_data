27ceaebfaf4bbfe34d74d60d2d5e4ed5da8f2206
Sergei <doctorradial@gmail.com>
2022-10-24 00:02:24 +0300

    Update main.blade.php

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -26,10 +26,10 @@
 
         <!-- Right -->
         <div>
-            <a class="me-4 text-reset" href="https://twitter.com/Seriiioga" target="_blank">
+            <a class="me-4" href="https://twitter.com/Seriiioga" target="_blank">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/2491px-Twitter-logo.svg.png" alt="" width="30" height="24">
             </a>
-            <a class="me-4 text-reset" href="https://t.me/seriiioga" target="_blank">
+            <a class="me-4 " href="https://t.me/seriiioga" target="_blank">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/2048px-Telegram_logo.svg.png" alt="" width="30" height="24">
             </a>
         </div>
